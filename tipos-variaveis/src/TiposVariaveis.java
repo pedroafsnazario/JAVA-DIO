1ac5d1ce7a0551d29d96368a51b6b7a5e5c693b4
pedroafsnazario <pedro905fernades@gmail.com>
2023-09-04 15:11:13 -0300

    Increment, Decrement e Inversão

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,19 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         
-        //Operadores.
-        //Unários, utilizados com os aritiméticos
-        //+ ou - (torna um valor positivo ou negativo).
-        // ++ ou -- (incrementa ou decrementa valores).
-         int numero = 5;
-          System.out.println(- numero); // gera '-5'.
-          System.out.println(numero); // gera '5'.
+        int numero = 5;
+        // x repeticao (incrementações e decrementações).
+        //operador de incremento funciona em certas ocasioes (como no sout).
+        // System.out.println(++ numero); 
 
-          //outro metodo de tornar um valor negativo seria
-          // numero = - numero; --> numero = numero * -1 (para converter pro positivo)
+        //numero = numero + 2; para abreviar ->
+        numero++; // igual a numero = numero + 1;
+
+        System.out.println(numero); // gera '6'.
+
+        //essas funcionalidades são iguais para a função decremental!!
 
 
+        //inversão '!'
 
+        boolean variavel = true;
+        System.out.println(!variavel); // fica como falsa.
+        //outro metodo é -> variavel = !variavel;
         
     }
 }
