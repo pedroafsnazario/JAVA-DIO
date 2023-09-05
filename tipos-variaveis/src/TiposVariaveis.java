0a18b53f7604779f4c02ba8176160194e961f2e1
pedroafsnazario <pedro905fernades@gmail.com>
2023-09-05 00:28:27 -0300

    Anotações sobre métodos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,29 +1,49 @@
-public class TiposVariaveis {
+ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        
-     //Lógicos
-     //permite criar expressões logicas maiores (boas para utilizar em condições e loops)
-     // '&&' operador logico 'E'
-     // '||' operador logico 'OU'
+   /*  
+     Métodos ->
+     uma classe é definida por atributos e métodos, já foi visto que atributos são, em sua grande maioria,
+     váriaveis de diferentes tipos e valores. Os métodos em sua vez, correspondem a funções ou sub-rotinas
+     disponiveis dentro de nossas classes.
 
-     boolean condicao1 = true;
-     boolean condicao2 = true;
+     Critérios a seguir (por questões de organizar e facilitar a leitura de código) ->
 
-     if(condicao1 && condicao2){
-        System.out.println("ambas condicoes são verdadeiras");
-     }
-     if(condicao1 && ( 7 > 4 )){
-        System.out.println("o valor booleano e a relação são verdadeiras");
-     }
-     else if(condicao1 || condicao2){
-        System.out.println("uma das condições é verdadeira");
-     }
-     else{
-        System.out.println("nenhuma delas é verdadeira");
-     }
+     - Deve ser nomeado como verbo
+     - Seguir padrão camelCase
 
-     System.out.println("termino de execucao");
+     exemplos ->
 
+     somar (int n1, int n2) {}
+
+     abrirConexao() {}
+
+     concluirProcessamento() {}
+
+     findById(int id) {}
+
+     -> Equivocado!
+
+     calcularImprimir() {} --> como assim "calcular Imprimir"? um metodo consegue fazer essas duas coisas distantes?
+     o responsavel a se fazer é criar dois metodos um pra calculo e outro pra impressao e ir vinculando os dois
+     caso seja nescessario o uso de ambos ao mesmo tempo.
+
+     O responsável é fazer método que realizem apenas um coisa, e nao duas!
+
+     Critério para Definir um Método ->
+
+     - Qual a proposta principal do método?
+
+     - Qual o tipo de retorno esperado após executar o método?
+
+     (caso um método não retorne um valor, ele será representado pela palavra chave 'void').
+     (ou seja, não terá de colocar a função 'return' no método :]).
+
+     - Quais parametros serão nescessários para execução do método?
+
+     - O método possui o risco de apresentar alguma exceção?
+
+     - Qual a visibilidade do método? (private/public)
+   */
 
     }
 }
