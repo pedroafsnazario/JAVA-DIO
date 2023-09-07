d3eeb1237580dc7c914ceab669ec4f98216ff5bc
pedroafsnazario <pedro905fernades@gmail.com>
2023-09-07 14:18:30 -0300

    Controle de Fluxo - Primeiras Aulas

---
+++ controle-de-fluxo/README.md
@@ -0,0 +1,4 @@
+## Control de Fluxo
+
+# Aqui está uma série de códigos simples feitas durante as video-aulas da DIO
+# Estes códigos foram feitos para exercitar o uso de controle de fluxo e compreender suas funções em um código (fiz algumas implementações nos códigos por curiosidade e testes próprios).
---
+++ controle-de-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,16 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 30.0;
+
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+
+            System.out.println("Você retirou de sua conta " + valorSolicitado + " Reais.");
+        }
+
+        
+        System.out.println("Agora tem " + saldo + " Reais de saldo na sua conta.");
+    }
+    
+}
