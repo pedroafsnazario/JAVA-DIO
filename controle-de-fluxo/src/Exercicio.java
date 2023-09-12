9967da134ca0e307280c383c1876c2a382950eae
pedroafsnazario <pedro905fernades@gmail.com>
2023-09-12 12:32:33 -0300

    exercicio basico

---
+++ controle-de-fluxo/src/Exercicio.java
@@ -0,0 +1,9 @@
+public class Exercicio {
+    public static void main(String[] args) {
+        boolean condicao = false;
+
+        while (condicao){
+            System.out.println("executou");
+        }
+    }
+}
